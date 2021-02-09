7f12e3985e2ce2b4eb9a5d5ea13a9818032b96fe
Admin <jithinks112@gmail.com>
2021-02-09 08:49:41 +0530

    modified numberline

--- p5.numberline/numberline.js
+++ p5.numberline/numberline.js
@@ -1,48 +1,53 @@
 class Numberline {
   constructor(config) {
-    const { x,y,w, min, max, tick } = config
+    const { x, y, w, min, max, tick } = config;
 
     this.x = x;
     this.y = y;
-    this.w = w
-    this.min = min
-    this.max = max
-    this.tick = tick
+    this.w = w;
+    this.min = min;
+    this.max = max;
+    this.tick = tick;
 
-    const range = this.w
-    const noOfTicks = (this.max - this.min)/this.tick
-    this.tickWidth = range/noOfTicks
+    const range = this.w;
+    const noOfTicks = (this.max - this.min) / this.tick;
+    this.tickWidth = range / noOfTicks;
   }
-  
+
   display() {
-    stroke(255)
-    strokeWeight(2)
-    line(this.x, this.y, this.x+this.w, this.y)
+    stroke(255);
+    strokeWeight(2);
+    line(this.x, this.y, this.x + this.w, this.y);
     this.markCoords();
   }
 
-  markCoords() {    
-    for(
-      let x=this.x, n = this.min; 
-      x <= this.x + this.w; 
+  markCoords() {
+    for (
+      let x = this.x, n = this.min;
+      x <= this.x + this.w;
       x += this.tickWidth, n += this.tick
     ) {
       noStroke();
-      fill(255)
+      fill(255);
 
-      circle(x,this.y, 6)      
+      circle(x, this.y, 6);
 
-      textAlign(CENTER)
+      textAlign(CENTER);
       textSize(16);
-      text(n, x, this.y+28);
+      text(n, x, this.y + 28);
     }
   }
 
-  getPix(x) {
-    return this.x + (x-this.min) * this.tickWidth * (1/this.tick)
+  getXPix(x) {
+    return this.x + (x - this.min) * this.tickWidth * (1 / this.tick);
+  }
+
+  getYPix(x) {
+    return this.y;
   }
 
   getCoord(p) {
-    return Math.round((this.tick*(p-this.x)/this.tickWidth) + this.min);
+    return Math.round((this.tick * (p - this.x)) / this.tickWidth + this.min);
   }
 }
+
